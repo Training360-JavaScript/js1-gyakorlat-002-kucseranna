33cda1a84d33b4b23cfdd00d872271ce576f2374
kucseranna <kucseranna@gmail.com>
2021-10-30 14:17:09 +0200

    kész

--- js/first.js
+++ js/first.js
@@ -4,3 +4,23 @@
 - A függvény szorozza meg a tömbelemek értékét 1.27-tel, majd pedig 
 __térjen vissza__ az elemek egész számra kerekített összegével!
 */
+
+// const brutto = (Int32Array) => {
+//     const itemsValueMultiplied = Int32Array.map(item => item ** 1.27);
+//     const sumOfValue = itemsValueMultiplied.reduce((previousValue, currentValue)=> previousValue + currentValue);
+//     return Math.round(sumOfValue)
+// }
+
+// //vagy
+
+// const brutto = IntArray  => IntArray.Math.round(intMultiplied.reduce(item ** 1.27))
+
+const brutto = (Uint16Array) => {
+    let bruttoFloatArray = Uint16Array.map(item => item * 1.27);
+    let bruttoIntArray = bruttoFloatArray.map(item => Math.round(item));
+    return bruttoIntArray.reduce((item1, item2) => item1 + item2);
+}
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@
 - A függvény neve `generateList` legyen!
 - A HTML-template egy lista, amelynek a listaelemei a kapott tömb értékeit tartalmazzák.
 */
+// const generateList = arrayString => {
+
+// }
+
+
+const generateList = (arrayOfStrings) => {
+    let listOfStrings = "";
+    arrayOfStrings.forEach(item => {
+        listOfStrings += "<li>" + item + "</li>";
+    });
+    return "<ul>" + listOfStrings + "</ul>"
+}
